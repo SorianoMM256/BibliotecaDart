90523ba72e52d7be891bef7f7cb852989be11bae
Matheus Motta Soriano <matheusmottasoriano@gmail.com>
2026-05-27 20:36:48 -0300

    adiciona classe livro

--- main.dart
+++ main.dart
@@ -16,6 +16,44 @@ abstract class ItemBiblioteca {
   void exibirDetalhes();
 }
 
+class Livro extends ItemBiblioteca {
+  String autor;
+  int isbn;
+
+  Livro({
+    required String titulo,
+    required int anoPublicacao,
+    required int quantidadeCopias,
+    required this.autor,
+    required this.isbn,
+  }) : super(
+          titulo: titulo,
+          anoPublicacao: anoPublicacao,
+          quantidadeCopias: quantidadeCopias,
+          custoBase: 15.00,
+          multaPorDiaAtraso: 2.50,
+        );
+
+  @override
+  void exibirDetalhes() {
+    print('Livro: $titulo');
+    print('Autor: $autor');
+    print('ISBN: $isbn');
+    print('Ano: $anoPublicacao');
+    print('Cópias disponíveis: $quantidadeCopias');
+    print('Custo base: R\$ $custoBase');
+    print('Multa por dia de atraso: R\$ $multaPorDiaAtraso');
+  }
+}
+
 void main() {
-  print('Sistema de Gestão Bibliotecária');
+  Livro livro = Livro(
+    titulo: 'Dom Casmurro',
+    anoPublicacao: 1899,
+    quantidadeCopias: 3,
+    autor: 'Machado de Assis',
+    isbn: 1001,
+  );
+
+  livro.exibirDetalhes();
 }
